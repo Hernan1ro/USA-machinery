20d163d31eb5c126243d2cf2f8ac9fb187df84bf
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-20 14:02:16 -0500

    adding global configuration and seo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,23 +3,27 @@ import Helmet from "react-helmet"
 import { Global, css } from "@emotion/react"
 import Header from "./header"
 import Footer from "./footer"
-import useSeo from "../hooks/useSeo"
 
 const Layout = ({ children }) => {
-  const seo = useSeo()
-  const {
-    siteName,
-    fallbackSeo: { description, title },
-  } = seo
   return (
     <>
       <Global
         styles={css`
+          :root {
+            --white: #fff;
+            --orange-primary: #f2b753;
+            --gray-primary: #32363b;
+            --ligt-gray: #c1c1c3;
+            --seconday-gray: #828387;
+            --warn-black: #33353b;
+            ---third-gray: #a7a8ac;
+          }
           * {
             box-sizing: border-box;
           }
           html {
             font-size: 62.5%;
+            scroll-behavior: smooth;
           }
           body {
             margin: 0;
@@ -50,8 +54,11 @@ const Layout = ({ children }) => {
         `}
       />
       <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
+        <title>USA MACHINERY</title>
+        <meta
+          name="description"
+          content="Empresa con mas de 10 años de experiencia en renta y venta de montacargas, venta de refacciones y servicio técnico a clientes con equipos"
+        />
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
           rel="stylesheet"
@@ -65,7 +72,7 @@ const Layout = ({ children }) => {
       </Helmet>
       <Header />
       {children}
-      <Footer title={title} />
+      <Footer title="USA MACHINERY" />
     </>
   )
 }

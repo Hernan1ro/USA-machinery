67c9b06b3c03d6c0f13eb864f7615cd3dcac92a4
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-23 08:40:57 -0500

    Html footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,55 +1,46 @@
 import React from "react"
 import { css } from "@emotion/react"
 import styled from "@emotion/styled"
-import Navegacion from "./nav"
-import { Link } from "gatsby"
 
-const EnlaceHome = styled(Link)`
-  color: #fff;
-  text-align: center;
-  text-decoration: none;
+const FooterContainer = styled.footer`
+  background-color: rgba(44, 62, 80);
+  margin-top: 5rem;
+  padding: 1rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `
 
-const Footer = ({ title }) => {
+const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <>
-      <footer
-        css={css`
-          background-color: rgba(44, 62, 80);
-          margin-top: 5rem;
-          padding: 1rem;
-        `}
-      >
-        <div
-          css={css`
-            max-width: 1200px;
-            margin: 0 auto;
-            @media (min-width: 768px) {
-              display: flex;
-              align-item: center;
-              justify-content: space-between;
-            }
-          `}
-        >
-          <EnlaceHome to="/">
-            <h1>Hotel Gatsby</h1>
-          </EnlaceHome>
+    <FooterContainer>
+      <h5>Quédate en contacto</h5>
+      <span>Compártenos tu correo</span>
+      <div>
+        <input type="text" />
+        <i>arrow</i>
+      </div>
+      <div>
+        <div>
+          <h6>Ubicación</h6>
+          <p>Río Pánuco 291 Nuevo Repueblo 64720 Monterrey, N.L.</p>
         </div>
-      </footer>
-      <p
-        id="nosotros"
-        css={css`
-          text-align: center;
-          color: #fff;
-          background-color: rgb(33, 44, 55);
-          margin: 0;
-          padding: 1rem;
-        `}
-      >
-        {title}, Todos los derechos Reservados {year} &copy;{" "}
+        <div>
+          <h6>Contacto</h6>
+          <p>
+            Horarios de Oficina <br /> 8:30 - 6:00 <br /> 81 2032 1618{" "}
+          </p>
+        </div>
+      </div>
+      <p>
+        {year} &copy; USA <br /> Montacargas
       </p>
-    </>
+    </FooterContainer>
   )
 }
 

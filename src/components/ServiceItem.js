26d6b48ff63a90b3bc1150563d9a9ca2f95dfc9d
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-22 19:23:53 -0500

    Creating service item component

---
+++ src/components/ServiceItem.js
@@ -0,0 +1,74 @@
+import React from "react"
+import styled from "@emotion/styled"
+import { css } from "@emotion/react"
+import { StaticImage } from "gatsby-plugin-image"
+import truckLogo from "../Icons/truck.svg"
+
+const ItemService = styled.div`
+  min-width: 23rem;
+  position: relative;
+  max-width: 235px;
+  span {
+    font-weight: 600;
+    font-size: 2.5rem;
+    color: var(--ligt-gray);
+  }
+  h3 {
+    font-weight: bolder;
+    color: var(--gray-primary);
+    line-height: 1;
+    margin-bottom: 1.2rem;
+  }
+  p {
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: var(--seconday-gray);
+  }
+`
+
+const ServiceItem = () => {
+  return (
+    <ItemService>
+      <span>01</span>
+      <h3>
+        Venta/Renta <br /> de Montacargas
+      </h3>
+      <StaticImage
+        placeholder="blurred"
+        layout="fixed"
+        width={210}
+        height={210}
+        src="../images/man-working.jpg"
+        alt="Man working"
+        css={css`
+          margin-bottom: 1rem;
+        `}
+      />
+      <div
+        css={css`
+          width: 50rem;
+          border-radius: 50%;
+          background-color: var(--orange-primary);
+          width: 5rem;
+          height: 5rem;
+          padding: 1rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          bottom: 105px;
+          right: 0px;
+          transform: scale(1.2);
+        `}
+      >
+        <img src={truckLogo} alt="truck icon" />
+      </div>
+      <p>
+        Con nosotros puedes rentar/comprar nuestros modelos de montacargas
+        nuevos, seminuevos y usados
+      </p>
+    </ItemService>
+  )
+}
+
+export default ServiceItem
--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,81 +1,30 @@
 import React from "react"
 import styled from "@emotion/styled"
-import { css } from "@emotion/react"
-import { StaticImage } from "gatsby-plugin-image"
-import truckLogo from "../Icons/truck.svg"
+
+import ServiceItem from "../components/ServiceItem"
 
 const ListServices = styled.ul`
   max-width: 1200px;
-  width: 80%;
+  width: 90%;
   margin: 4rem auto 0 auto;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 3rem;
-  overflow: hidden;
-`
-const ServiceItem = styled.div`
-  min-width: 23rem;
-  position: relative;
-  span {
-    font-weight: 600;
-    font-size: 2.5rem;
-    color: var(--ligt-gray);
-  }
-  h3 {
-    font-weight: bolder;
-    color: var(--gray-primary);
-    line-height: 1;
-    margin-bottom: 1.2rem;
-  }
-  p {
-    font-size: 1.6rem;
-    font-weight: 600;
-    color: var(--seconday-gray);
+  overflow: scroll;
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, 235px);
+    overflow: hidden;
+    justify-content: center;
   }
 `
 
 const ServiceList = () => {
   return (
     <ListServices>
-      <ServiceItem>
-        <span>01</span>
-        <h3>
-          Venta/Renta <br /> de Montacargas
-        </h3>
-        <StaticImage
-          placeholder="blurred"
-          layout="fixed"
-          width={210}
-          height={210}
-          src="../images/man-working.jpg"
-          alt="Man working"
-          css={css`
-            margin-bottom: 1rem;
-          `}
-        />
-        <div
-          css={css`
-            width: 50rem;
-            border-radius: 50%;
-            background-color: var(--orange-primary);
-            width: 5rem;
-            height: 5rem;
-            padding: 1rem;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            bottom: 105px;
-            right: 0px;
-          `}
-        >
-          <img src={truckLogo} alt="truck icon" />
-        </div>
-        <p>
-          Con nosotros puedes rentar/comprar nuestros modelos de montacargas
-          nuevos, seminuevos y usados
-        </p>
-      </ServiceItem>
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
     </ListServices>
   )
 }

1f619c31f41efaf1abf6949cad96c231351fb658
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-23 20:38:03 -0500

    Footer desktop view

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,10 @@ import React from "react"
 import { css } from "@emotion/react"
 import styled from "@emotion/styled"
 
-const FooterContainer = styled.footer`
+const FooterContainer = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: var(--gray-primary);
   margin-top: 2rem;
-  padding: 2rem;
-  max-width: 1200px;
-  margin: 0 auto;
   i {
     font-size: 2.6rem;
     color: var(--white);
@@ -35,67 +31,91 @@ const FooterContainer = styled.footer`
     font-weight: bold;
   }
   @media (min-width: 768px) {
-    align-items: center;
-    justify-content: space-between;
+    margin-left: 40%;
   }
 `
 
 const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <FooterContainer>
-      <span
-        css={css`
-          color: var(--white);
-          font-weight: 700;
-          font-size: 2rem;
-        `}
-      >
-        Quédate en contacto
-      </span>
-      <span
-        css={css`
-          color: var(--ligt-gray);
-          font-weight: 700;
-          font-size: 2rem;
-        `}
-      >
-        Compártenos tu correo
-      </span>
-      <div
-        css={css`
-          height: 5rem;
-          border-bottom: 3px solid var(--white);
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        `}
-      >
-        <input type="text" />
-        <i class="fas fa-arrow-right"></i>
-      </div>
-      <div
-        css={css`
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-          grid-column-gap: 1rem;
-        `}
-      >
-        <div>
-          <h6>Ubicación</h6>
-          <p>Río Pánuco 291 Nuevo Repueblo 64720 Monterrey, N.L.</p>
+    <footer
+      css={css`
+        background-color: var(--gray-primary);
+        max-width: 1200px;
+        padding: 2rem 2rem 4rem 2rem;
+        margin: 0 auto;
+        position: relative;
+        @media (min-width: 768px) {
+          padding: 2rem 2rem 12rem 2rem;
+        }
+      `}
+    >
+      <FooterContainer>
+        <span
+          css={css`
+            color: var(--white);
+            font-weight: 700;
+            font-size: 2rem;
+          `}
+        >
+          Quédate en contacto
+        </span>
+        <span
+          css={css`
+            color: var(--ligt-gray);
+            font-weight: 700;
+            font-size: 2rem;
+          `}
+        >
+          Compártenos tu correo
+        </span>
+        <div
+          css={css`
+            height: 5rem;
+            border-bottom: 3px solid var(--white);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            max-width: 600px;
+            @media (min-width: 768px) {
+              margin-bottom: 8rem;
+            }
+          `}
+        >
+          <input type="text" />
+          <i class="fas fa-arrow-right"></i>
         </div>
-        <div>
-          <h6>Contacto</h6>
-          <p>
-            Horarios de Oficina <br /> 8:30 - 6:00 <br /> 81 2032 1618{" "}
-          </p>
+        <div
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-column-gap: 1rem;
+          `}
+        >
+          <div>
+            <h6>Ubicación</h6>
+            <p>Río Pánuco 291 Nuevo Repueblo 64720 Monterrey, N.L.</p>
+          </div>
+          <div>
+            <h6>Contacto</h6>
+            <p>
+              Horarios de Oficina <br /> 8:30 - 6:00 <br /> 81 2032 1618{" "}
+            </p>
+          </div>
         </div>
-      </div>
-      <p>
-        &copy; {year} USA <br /> Montacargas
-      </p>
-    </FooterContainer>
+        <p
+          css={css`
+            @media (min-width: 768px) {
+              position: absolute;
+              left: 7rem;
+              bottom: 4rem;
+            }
+          `}
+        >
+          &copy; {year} USA Montacargas
+        </p>
+      </FooterContainer>
+    </footer>
   )
 }
 

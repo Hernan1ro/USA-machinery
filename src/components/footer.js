a5d33155698589787b4f651cd96a21e1475626dc
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-23 20:00:05 -0500

    Footer movil vew

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,38 @@ import { css } from "@emotion/react"
 import styled from "@emotion/styled"
 
 const FooterContainer = styled.footer`
-  background-color: rgba(44, 62, 80);
-  margin-top: 5rem;
-  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--gray-primary);
+  margin-top: 2rem;
+  padding: 2rem;
   max-width: 1200px;
   margin: 0 auto;
+  i {
+    font-size: 2.6rem;
+    color: var(--white);
+  }
+  input {
+    height: inherit;
+    width: -webkit-fill-available;
+    background-color: var(--gray-primary);
+    border: none;
+    border-bottom: 2px solid var(--white);
+    outline: none;
+  }
+  h6 {
+    font-size: 2rem;
+    color: var(--white);
+    margin: 2rem 0 0 0;
+  }
+  p {
+    line-height: 1;
+    color: var(---third-gray);
+    font-size: 1.6rem;
+    margin: 1rem 0;
+    font-weight: bold;
+  }
   @media (min-width: 768px) {
-    display: flex;
     align-items: center;
     justify-content: space-between;
   }
@@ -19,13 +44,43 @@ const Footer = () => {
   const year = new Date().getFullYear()
   return (
     <FooterContainer>
-      <h5>Quédate en contacto</h5>
-      <span>Compártenos tu correo</span>
-      <div>
+      <span
+        css={css`
+          color: var(--white);
+          font-weight: 700;
+          font-size: 2rem;
+        `}
+      >
+        Quédate en contacto
+      </span>
+      <span
+        css={css`
+          color: var(--ligt-gray);
+          font-weight: 700;
+          font-size: 2rem;
+        `}
+      >
+        Compártenos tu correo
+      </span>
+      <div
+        css={css`
+          height: 5rem;
+          border-bottom: 3px solid var(--white);
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        `}
+      >
         <input type="text" />
-        <i>arrow</i>
+        <i class="fas fa-arrow-right"></i>
       </div>
-      <div>
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-column-gap: 1rem;
+        `}
+      >
         <div>
           <h6>Ubicación</h6>
           <p>Río Pánuco 291 Nuevo Repueblo 64720 Monterrey, N.L.</p>
@@ -38,7 +93,7 @@ const Footer = () => {
         </div>
       </div>
       <p>
-        {year} &copy; USA <br /> Montacargas
+        &copy; {year} USA <br /> Montacargas
       </p>
     </FooterContainer>
   )
